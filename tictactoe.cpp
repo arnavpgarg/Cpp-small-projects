0065dacae5d35622957e0ed9d66966a9a94dab7c
Arnav P Garg <151569871+arnavpgarg@users.noreply.github.com>
2025-07-16 14:17:02 +0530

    Add files via upload

---
+++ tictactoe.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+
+void drawBoard(char *spaces);
+void playerMove(char *spaces, char player);
+void computerMove(char *spaces, char computer);
+bool chooseWinner(char *spaces, char player, char computer);
+bool checkTie(char *spaces, char player, char computer);
+
+int main() {
+    char spaces[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+    char player = 'X';
+    char computer = 'O';
+    bool running = true;
+
+    drawBoard(spaces);
+
+    while(running) {
+        playerMove(spaces, player);
+        drawBoard(spaces);
+
+        if(chooseWinner(spaces, player, computer)){
+            running = false;
+            break;
+        } else if (checkTie(spaces)) {
+            running  = false;
+            break;
+        }
+
+        computerMove(spaces, computer);
+        drawBoard(spaces);
+
+        if(chooseWinner(spaces, player, computer)){
+            running = false;
+            break;
+        } else if (checkTie(spaces)) {
+            running  = false;
+            break;
+        }
+    }
+    
+    return 0;
+}
+
+void drawBoard(char *spaces) {
+    cout << '\n';
+    cout << "     |     |     " << '\n';
+    cout << "  " << spaces[0] << "  |  " << spaces[1] << "  |  " << spaces[2] << '\n';
+    cout << "_____|_____|_____" << '\n';
+    cout << "     |     |     " << '\n';
+    cout << "  " << spaces[3] << "  |  " << spaces[4] << "  |  " << spaces[5] << '\n';
+    cout << "_____|_____|_____" << '\n';
+    cout << "     |     |     " << '\n';
+    cout << "  " << spaces[6] << "  |  " << spaces[7] << "  |  " << spaces[8] << '\n';
+    cout << "     |     |     " << '\n';
+    cout << '\n';
+}
+void playerMove(char *spaces, char player) {
+    int number;
+    do {
+        cout << "Enter a spot to place the marker(1-9): ";
+        cin >> number;
+        number--;
+
+        if(spaces[number] == ' ') {
+            spaces[number] = player;
+            break;
+        }
+    } while(!number > 0 || !number < 8);
+
+}
+void computerMove(char *spaces, char computer) {
+    int number;
+    srand(time(0));
+
+    while(true) {
+        number = rand() % 9;
+        if(spaces[number] == ' '){
+            spaces[number] = computer;
+            break;
+        }
+    }
+
+}
+bool chooseWinner(char *spaces, char player, char computer){
+
+    if((spaces[0] != ' ') && (spaces[0] == spaces[1]) && (spaces[1] == spaces[2])){
+        spaces[0] == player ? cout << "you win!\n" : cout << "you lose!\n";
+
+    } else if ((spaces[3] != ' ') && (spaces[3] == spaces[4]) && (spaces[4] == spaces[5])){
+        spaces[3] == player ? cout << "you win!\n" : cout << "you lose!\n";
+    } else  if((spaces[6] != ' ') && (spaces[6] == spaces[7]) && (spaces[7] == spaces[8])){
+        spaces[6] == player ? cout << "you win!\n" : cout << "you lose!\n"; 
+    } else  if((spaces[0] != ' ') && (spaces[0] == spaces[3]) && (spaces[3] == spaces[6])){
+        spaces[0] == player ? cout << "you win!\n" : cout << "you lose!\n"; 
+    } else  if((spaces[1] != ' ') && (spaces[1] == spaces[4]) && (spaces[4] == spaces[7])){
+        spaces[1] == player ? cout << "you win!\n" : cout << "you lose!\n"; 
+    } else  if((spaces[2] != ' ') && (spaces[2] == spaces[5]) && (spaces[5] == spaces[8])){
+        spaces[2] == player ? cout << "you win!\n" : cout << "you lose!\n";
+    } else  if((spaces[0] != ' ') && (spaces[0] == spaces[4]) && (spaces[4] == spaces[8])){
+        spaces[0] == player ? cout << "you win!\n" : cout << "you lose!\n";
+    } else  if((spaces[2] != ' ') && (spaces[2] == spaces[4]) && (spaces[4] == spaces[6])){
+        spaces[2] == player ? cout << "you win!\n" : cout << "you lose!\n";
+    } else {
+        return false;
+    }
+    return true;
+}
+bool checkTie(char *spaces) {
+
+    for(int i = 0; i < 9; i++) {
+        if(spaces[i] == ' ') {
+            return false;
+        }
+    }
+    cout << "It's a tie!\n";
+    return true;
+}
